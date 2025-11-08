b82fa92f23209b85981f0a62f5e32e154037b8f1
Kuzivakwashe C Kanyemba <claseyk@gmail.com>
2025-11-08 20:00:33 +0200

    all files update

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ showSlides();
 function showSlides() {
     let slides = document.getElementsByClassName("slides");
     for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].classList.remove("active");
     }
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1; }
-    if(slides.length > 0) slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
+    slides[slideIndex - 1].classList.add("active");
+    setTimeout(showSlides, 3000); // change every 3 seconds
 }
+
 
 // Optional: Add next/prev functionality
 function plusSlides(n) {
